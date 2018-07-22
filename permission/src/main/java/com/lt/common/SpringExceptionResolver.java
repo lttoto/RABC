5c8c5111697372247824f50750d4fe8637a5c220
liutao <18616948061@163.com>
2018-07-23 03:01:58 +0800

    global exception SpringExceptionResovler

---
+++ permission/src/main/java/com/lt/common/SpringExceptionResolver.java
@@ -0,0 +1,43 @@
+package com.lt.common;
+
+import com.lt.exception.PermissionException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by taoshiliu on 2018/7/23.
+ */
+@Slf4j
+public class SpringExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        String url = httpServletRequest.getRequestURL().toString();
+        ModelAndView mv;
+        String defaultMsg = "System error";
+
+        //.json 接口请求,.page 页面请求
+        if (url.endsWith(".json")) {
+            if(e instanceof PermissionException) {
+                JsonData result = JsonData.fail(e.getMessage());
+                mv = new ModelAndView("jsonView",result.toMap());
+            }else {
+                log.error("unknow json exception,url:" + url,e);
+                JsonData result = JsonData.fail(defaultMsg);
+                mv = new ModelAndView("jsonView",result.toMap());
+            }
+        }else if (url.endsWith(".page")){
+            log.error("unknow page exception,url:" + url,e);
+            JsonData result = JsonData.fail(defaultMsg);
+            mv = new ModelAndView("exception",result.toMap());
+        }else  {
+            log.error("unknow exception,url:" + url,e);
+            JsonData result = JsonData.fail(defaultMsg);
+            mv = new ModelAndView("jsonView",result.toMap());
+        }
+        return mv;
+    }
+}
---
+++ permission/src/main/java/com/lt/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.lt.controller;
+
+import com.lt.common.JsonData;
+import com.lt.exception.PermissionException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by taoshiliu on 2018/7/23.
+ */
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/hello.json")
+    @ResponseBody
+    public JsonData hello() {
+        log.info("hello");
+        throw new RuntimeException("test exception");
+        //return JsonData.success("hello permission");
+    }
+}
